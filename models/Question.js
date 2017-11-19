59b2c8815fad8fdf3359b73bc6c02186f76e7f37
Helen Liutongco <bellpickle@gmail.com>
2017-11-19 04:30:26 +0000

    automatically backed up by learn

--- models/Question.js
+++ models/Question.js
@@ -33,7 +33,7 @@ static Find(id){
   const sql=`SELECT * FROM questions WHERE id = ? LIMIT 1`
   return new Promise(function(resolve){
     db.get(sql,[id],function(err,result){
-      const question=new Question(result.content)
+      const question=new Question()
       question.content=result.content
       question.id=result.id;
       resolve(question);
